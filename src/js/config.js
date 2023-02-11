f08348d7ec41458d60ad2d613c8c120dd1891f9b
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2023-02-11 10:32:33 +0100

    continue implementing features for uploading a new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,7 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a6cfb17d-d546-41bb-9f91-fefe4a5524be';
+export const MODAL_CLOSE_SEC = 2.5;
+
+// https://forkify-api.herokuapp.com/api/v2/recipes?key=a6cfb17d-d546-41bb-9f91-fefe4a5524be
--- src/js/controller.js
+++ src/js/controller.js
@@ -22,6 +22,7 @@ I hope this helps and solves any issues you might find. Let me know in the Q&A i
 ///////////////////////////////////////
 
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -117,10 +118,29 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
 
-  // Upload new recipe data
+    // Upload new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Close form window
+    setTimeout(() => {
+      // addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('🛑', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 // Publisher-subscriber pattern
@@ -134,8 +154,3 @@ const init = function () {
   addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
-
-const clearBookmarks = function () {
-  localStorage.clear('bookmarks');
-};
-// clearBookmarks();
